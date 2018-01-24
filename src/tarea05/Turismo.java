e5bb336234cb298712bb454a5f250519d6b1ad93
john <john@john-X555LAB>
2018-01-24 18:42:33 +0100

    Se crean los métodos get de los atributos.

--- src/tarea05/Turismo.java
+++ src/tarea05/Turismo.java
@@ -42,4 +42,29 @@ public class Turismo {
 		
 		return emparejador.matches();
 	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+	
+	public boolean getDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+	
 }
