2ce9dc064a9c139b9820dc516f47e26c7efe6f8b
john <john@john-X555LAB>
2018-01-24 19:14:57 +0100

    Se crea el constructor con parámetros.

--- src/tarea05/Alquiler.java
+++ src/tarea05/Alquiler.java
@@ -10,5 +10,19 @@ public class Alquiler {
 	private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat ("dd/MM/yyyy HH:mm");
 	private final int MS_DIA = 1000*60*60*24;
 	private final double PRECIO_DIA = 30.0;
+	private Cliente cliente;
+	private Turismo turismo;
+	private boolean disponible;
+	
+	
+	public Alquiler (Cliente cliente, Turismo turismo) {
+		
+		this.cliente = cliente;
+		this.turismo = turismo;
+		fecha = new Date();
+		dias = 0;
+		disponible = false;
+		
+	}
 
 }
