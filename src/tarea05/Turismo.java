fbd5aa1f0183637c47a560a3c079bba4d1d4c5a5
john <john@john-X555LAB>
2018-01-24 18:09:21 +0100

    Se crea la clase turismo con los atributos.

---
+++ src/tarea05/Turismo.java
@@ -0,0 +1,14 @@
+package tarea05;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Turismo {
+	
+	private String matricula;
+	private String marca;
+	private String modelo;
+	private int cilindrada;
+	private boolean disponible;
+	
+}
