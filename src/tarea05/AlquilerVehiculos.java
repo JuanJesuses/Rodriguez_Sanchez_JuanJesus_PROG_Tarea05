1d5c311f8b588779b350864a1227ac2db7bdac2c
john <john@john-X555LAB>
2018-01-24 20:03:21 +0100

    Se crea la clase AlquilerVehiculos.

---
+++ src/tarea05/AlquilerVehiculos.java
@@ -0,0 +1,13 @@
+package tarea05;
+
+public class AlquilerVehiculos {
+	
+	private final int MAX_TURISMOS = Integer.MAX_VALUE;
+	private final int MAX_CLIENTES = Integer.MAX_VALUE;
+	private final int MAX_ALQUILERES = Integer.MAX_VALUE;
+	
+	private Turismo[] turismos;
+	private Cliente[] clientes;
+	private Alquiler[] alquileres;
+	
+}
